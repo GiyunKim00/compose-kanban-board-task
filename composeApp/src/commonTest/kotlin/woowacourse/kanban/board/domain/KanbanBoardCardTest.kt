a8253b98d90c5e04559b5765c4b55e24d3552263
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-07 19:52:40 +0900

    feat: KanbanBoardCard 정보에 따른 UI 출력 테스트 로직 작성 - 모든 필드가 유효한 경우 - 최소 필드만 유효한 경우

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/domain/KanbanBoardCardTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/domain/KanbanBoardCardTest.kt
@@ -1,6 +1,13 @@
 package woowacourse.kanban.board.domain
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.runComposeUiTest
+import woowacourse.kanban.board.ui.KanbanBoardCard
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -94,4 +101,46 @@ class KanbanCardDataTest {
 }
 
 @OptIn(ExperimentalTestApi::class)
-class KanbanBoardCardTest {}
+class KanbanBoardCardTest {
+
+    @Test
+    fun `모든 필드가 요구 사항대로 입력된 경우, 모든 내용이 노출된다`() = runComposeUiTest {
+        val cardData = KanbanCardData.create(
+            title = "제목",
+            content = "내용",
+            tags = listOf("태그1", "태그2", "태그3", "태그4", "태그5"),
+            accountName = "테스트 계정"
+        )
+
+        setContent {
+            KanbanBoardCard(kanbanCardData = cardData)
+        }
+
+        onNodeWithText("제목").assertIsDisplayed()
+        onNodeWithText("내용").assertIsDisplayed()
+        onNodeWithText("태그1").assertIsDisplayed()
+        onNodeWithText("태그2").assertIsDisplayed()
+        onNodeWithText("태그3").assertIsDisplayed()
+        onNodeWithText("태그4").assertIsDisplayed()
+        onNodeWithText("태그5").assertIsDisplayed()
+        onNodeWithText("테스트 계정").assertIsDisplayed()
+    }
+
+    @Test
+    fun `제목과 계정 정보만으로 카드를 구성할 수 있다`() = runComposeUiTest {
+        val cardData = KanbanCardData.create(
+            title = "제목",
+            content = "",
+            tags = emptyList(),
+            accountName = "테스트 계정"
+        )
+
+        setContent {
+            KanbanBoardCard(kanbanCardData = cardData)
+        }
+        onNodeWithContentDescription("Kanban Card Title").assertIsDisplayed()
+        onNodeWithContentDescription("Kanban Card Content").assertDoesNotExist()
+        onAllNodesWithContentDescription("Kanban Card Tag").assertCountEquals(0)
+        onNodeWithContentDescription("Kanban Card Account Info").assertIsDisplayed()
+    }
+}
